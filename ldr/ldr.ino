faf29b492379b3a113c231d6173309128c03c8b7
Hakan Yıldız <hknyldz93@gmail.com>
2016-02-01 10:44:38 +0200

    ldr changed

--- ldr/ldr.ino
+++ ldr/ldr.ino
@@ -1,11 +1,14 @@
 
 const int sensorPin = A0 ;
-
+const int ledPin = 3;
 void setup(){
   Serial.begin(9600);
+  
 }
 void loop(){
   int blinkRate = analogRead(sensorPin);
+  blinkRate = map(blinkRate, 0,1023,0,255);
+  analogWrite(ledPin, 255 - blinkRate);
   Serial.println(blinkRate);
   delay(500);
   
